9d80a2324dc271f9db56369a71f6db54b25f88d7
Mateusz Gomolka <mdm.gomolka@gmail.com>
2023-01-23 20:29:38 +0000

    Change functions to classes

--- src/components/BooksList/BooksList.js
+++ src/components/BooksList/BooksList.js
@@ -1,9 +1,13 @@
-const BooksList = ({ books, removeBook }) => {
-  return (
+import React from "react";
+
+class BooksList extends React.Component {
+  render() {return (
     <ul>
-      {books.map(book => <li key={book.id}>{book.title} by {book.author} <button onClick={() => removeBook(book.id)}>Remove</button></li>)}
+      {this.props.books.map(book => <li key={book.id}>{book.title} by {book.author} <button onClick={() => this.props.removeBook(book.id)}>Remove</button></li>)}
     </ul>
-  );
+  );}
+
+
 };
 
 export default BooksList;
